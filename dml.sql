e2ab1def476f04fa7360aedadd3d6c16f4f352e9
morenyangbot <45971234+morenyangbot@users.noreply.github.com>
2019-07-03 12:10:38 +0800

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -1,7 +1,17 @@
--- 插入记录  
+use student_examination_sys;
+-- 插入记录
 
--- 修改记录  
+insert into student (id, name, age, sex)
+values ('1901', '小明', 10, '男');
 
---  删除记录  
+-- 修改记录
+update student
+set name = '大明'
+where id like '1901';
 
--- 查询记录  
+-- 删除记录
+delete from student
+where id like '1901';
+
+-- 查询记录
+select * from student where id like '1901'
